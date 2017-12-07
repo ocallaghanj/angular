d52d57f1de2cf703ec9e4e934eed9d1a99ed2231
ocallaghanj <jarrod.ocallaghan@surefiresystems.com>
2017-12-08 10:02:58 +1100

    6. Services

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,18 +11,18 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
+  constructor(private heroService: HeroService) { }  
+
+  ngOnInit() {
+  this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor(private heroService: HeroService) { }  
-
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit() {
-  this.getHeroes();
   }
 }
