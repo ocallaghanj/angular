ce40cacb6cc22140fd4d8e1c7096a0ea697461fb
ocallaghanj <jarrod.ocallaghan@surefiresystems.com>
2018-01-04 12:42:26 +1100

    Finished tutorial (css missing)

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-ap
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
